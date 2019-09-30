010aaa661c86f78444fcd23df063174a64819d7e
calmmy <jifanm3@gmail.com>
2019-10-01 00:31:43 +0900

    senier project start

---
+++ server/server/server/CProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class CProgram
+    {
+        // 프로그램 메인함수
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
